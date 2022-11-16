7942d27db55a751ddf85f7f06c9a24319a9d561a
Helin Faidhan <helinkurd1997@gmail.com>
2022-11-16 21:43:30 +0300

    assignment completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const newTaskForm = document.getElementById("create-task-form");
+
+
+  const taskList = document.getElementById("tasks")
+
+ 
+  newTaskForm.addEventListener("submit", (e) => {
+    e.preventDefault()
+    const newTaskInput = e.target.querySelector("#new-task-description").value
+    const taskPTag = document.createElement('p')
+    taskPTag.textContent = newTaskInput 
+    taskList.appendChild(taskPTag)
+    console.log(taskList)
+    console.log(newTaskInput)
+  })
+
+
+
 });
